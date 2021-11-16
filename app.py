b5ef551370bd55205ad2890ae9eea200c08c8adf
PedroSoldera <pedro-soldera2011@hotmail.com>
2021-11-16 21:29:10 +0000

    v3

--- app.py
+++ app.py
@@ -17,14 +17,23 @@ def adicionar():
     return render_template('adicionar.html')
 
 @app.route('/salvar', methods=['POST'])
-def save():
+def salvar():
     filme = request.form['filme']
-    nota_pub = request.form['nota_publico']
-    nota_crit = request.form['nota_critica']
-    novos_filmes = {'Nome filme:':f'{filme}', 'Nota Público': f'{nota_pub}', 'Nota Critica': f'{nota_crit}'}
+    nota_publico = request.form['nota_publico']
+    nota_critica = request.form['nota_critica']
+    novos_filmes = {'Nome filme:':f'{filme}', 'Nota Público': f'{nota_publico}', 'Nota Critica': f'{nota_critica}'}
 
     filmes.append(novos_filmes)
     
-    return redirect('https://5000-lavender-jay-yx2ss8o4.ws-us18.gitpod.io/')
+    return redirect('https://5000-coral-mosquito-1pmn8i18.ws-us18.gitpod.io/')
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    lista_filme = []
+    busca = request.form['busca']
+    for filme in filmes: 
+        if busca.lower() in filme['filme'].lower():
+            lista_filme.append(filme)
+    return render_template('buscar.html', lista_filme=lista_filme)
 
 app.run(debug=True)
