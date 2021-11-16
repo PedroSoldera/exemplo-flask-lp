eaf15803b8b4c5f3b5dfa849a365dc5bc59b5848
PedroSoldera <pedro-soldera2011@hotmail.com>
2021-11-16 19:40:23 +0000

    meio-projeto

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html', nome='Fulano')
+filmes = [
+    {"filme": "Star Wars - A vingança dos Sith", "nota_publico": 60, "nota_critica": 80},
+    {"filme": "Star Wars - A Ameaça Fantasma", "nota_publico": 59, "nota_critica": 52},
+    {"filme": "Star Wars - O Ataque dos Clones", "nota_publico": 56, "nota_critica": 65},
+]
 
-@app.route('/bye')
-def bye():
-    return render_template('tchau.html')
+@app.route('/')
+def index():
+    return render_template('index.html', lista=filmes)
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/save', methods=['POST'])
+def save():
+    filme = request.form['filme']
+    filme = request.form['nota_publico']
+    filme = request.form['nota_critica']
+    novos_filmes = { 'Nome filme:':f'{filme}', 'Nota Público'}
+
+    return redirect('https://5000-brown-lemur-kiu2j61j.ws-us18.gitpod.io/')
 
 app.run(debug=True)
