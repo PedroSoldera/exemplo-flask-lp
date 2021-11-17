fc37062c6ed610ab579db19aa640f1a829e61388
PedroSoldera <pedro-soldera2011@hotmail.com>
2021-11-17 18:56:17 +0000

    s

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 filmes = [
-    {"filme": "Star Wars - A vingança dos Sith", "nota_publico": 60, "nota_critica": 80},
-    {"filme": "Star Wars - A Ameaça Fantasma", "nota_publico": 59, "nota_critica": 52},
-    {"filme": "Star Wars - O Ataque dos Clones", "nota_publico": 56, "nota_critica": 65},
+    {"nome_filme": "Star Wars - A vingança dos Sith", "nota_publico": 60, "nota_critica": 80},
+    {"nome_filme": "Star Wars - A Ameaça Fantasma", "nota_publico": 59, "nota_critica": 52},
+    {"nome_filme": "Star Wars - O Ataque dos Clones", "nota_publico": 56, "nota_critica": 65},
 ]
 
 @app.route('/')
@@ -18,14 +18,14 @@ def adicionar():
 
 @app.route('/salvar', methods=['POST'])
 def salvar():
-    filme = request.form['filme']
+    nome_filme = request.form['nome_filme']
     nota_publico = request.form['nota_publico']
     nota_critica = request.form['nota_critica']
-    novos_filmes = {'Nome filme:':f'{filme}', 'Nota Público': f'{nota_publico}', 'Nota Critica': f'{nota_critica}'}
+    novos_filmes = {'nome_filme': nome_filme, 'nota_publico': nota_publico, 'nota_critica': nota_critica}
 
     filmes.append(novos_filmes)
     
-    return redirect('https://5000-coral-mosquito-1pmn8i18.ws-us18.gitpod.io/')
+    return redirect('https://5000-amaranth-moose-j937nk9u.ws-us18.gitpod.io/')
 
 @app.route('/buscar', methods=['POST'])
 def buscar():
@@ -35,5 +35,7 @@ def buscar():
         if busca.lower() in filme['filme'].lower():
             lista_filme.append(filme)
     return render_template('buscar.html', lista_filme=lista_filme)
+
+
 
 app.run(debug=True)
